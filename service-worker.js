2ca1c09fcd836cd3b09bf2e198959749a05b1094
Ivan Peña <ipena@bancodebogota.com.co>
2020-08-03 10:38:59 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ciclopes-movi/cyclope-landings-front/precache-manifest.84d1129ebf9164b14ad1411581256b66.js"
+  "/ciclopes-movi/cyclope/precache-manifest.d4f5a72eeb33caef33f815cf284302dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ciclopes-movi/cyclope-landings-front/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ciclopes-movi/cyclope/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
